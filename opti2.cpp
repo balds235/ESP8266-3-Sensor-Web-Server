9a4acdda8d1766fed520120992b1eb6428dc0388
Xavi Quezada <167028963+balds235@users.noreply.github.com>
2024-04-18 11:40:01 -0700

    optimized opti2

--- opti2.cpp
+++ opti2.cpp
@@ -1,39 +1,31 @@
 #include <ESP8266WiFi.h>
 const char *ssid = "SSID";
 const char *password = "PASSWORD";
-#define MOISTURE_PIN A0 // Analog pin connected to the moisture sensor
-#define TRIGGER_PIN D3  // Digital pin connected to the HC-SR04 trigger
-#define ECHO_PIN D4     // Digital pin connected to the HC-SR04 echo
+#define MOISTURE_PIN A0
+#define TRIGGER_PIN D3
+#define ECHO_PIN D4
 #define SERVER_PORT 80
 WiFiServer server(SERVER_PORT);
 void setup() {
   Serial.begin(9600);
   WiFi.begin(ssid, password);
-  Serial.print("Connecting to WiFi");
   while (WiFi.status() != WL_CONNECTED) {
     delay(1000);
-    Serial.print(".");
   }
-  Serial.println("\nConnected to WiFi");
   server.begin();
-  Serial.println("TCP server started");
-  dht.begin();
   pinMode(TRIGGER_PIN, OUTPUT);
   pinMode(ECHO_PIN, INPUT);
 }
 void handleClientRequest(WiFiClient client) {
   String request = client.readStringUntil('\r');
-  Serial.println("Received request: " + request);
-  float temperature = dht.readTemperature();
   int moistureLevel = analogRead(MOISTURE_PIN);
   long duration, distance;
   digitalWrite(TRIGGER_PIN, LOW);
-  delayMicroseconds(2);
   digitalWrite(TRIGGER_PIN, HIGH);
   delayMicroseconds(10);
   digitalWrite(TRIGGER_PIN, LOW);
   duration = pulseIn(ECHO_PIN, HIGH);
-  distance = duration * 0.034 / 2;
+  distance = duration * 0.017;
   client.println("HTTP/1.1 200 OK");
   client.println("Content-Type: text/plain");
   client.println();
@@ -43,12 +35,10 @@ void handleClientRequest(WiFiClient client) {
   client.print(distance);
   client.println(" cm");
   client.stop();
-  Serial.println("Client disconnected");
 }
 void loop() {
   WiFiClient client = server.available();
   if (client) {
-    Serial.println("Client connected");
     handleClientRequest(client);
   }
 }
