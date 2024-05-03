83c36806c3e362f903be489846b71e1cddf538bf
Xavi Quezada <167028963+balds235@users.noreply.github.com>
2024-05-03 12:03:11 -0700

    IT IS HIM!!!

--- opti2.cpp
+++ opti2.cpp
@@ -1,6 +1,7 @@
 #include <DHT.h>
 #include <ESP8266WiFi.h>
 #include <WebSocketsServer.h>
+
 const char *ssid = "YOUR_SSID";
 const char *password = "YOUR_PASSWORD";
 #define DHTPIN D2
@@ -9,22 +10,23 @@ const char *password = "YOUR_PASSWORD";
 #define TRIGGER_PIN D3
 #define ECHO_PIN D4
 #define SERVER_PORT 80
+
 WebSocketsServer webSocket = WebSocketsServer(SERVER_PORT);
 DHT dht(DHTPIN, DHTTYPE);
-void webSocketEvent(uint8_t num, WStype_t type, uint8_t *payload,
-                    size_t length) {
+
+void webSocketEvent(uint8_t num, WStype_t type, uint8_t *payload, size_t length) {
   switch (type) {
-  case WStype_DISCONNECTED:
-    Serial.printf("[%u] Disconnected!\n", num);
-    break;
-  case WStype_CONNECTED:
-    Serial.printf("[%u] Connected from IP: %s\n", num,
-                  webSocket.remoteIP(num).toString().c_str());
-    Serial.print("My IP address: ");
-    Serial.println(WiFi.localIP());
-    break;
+    case WStype_DISCONNECTED:
+      Serial.printf("[%u] Disconnected!\n", num);
+      break;
+    case WStype_CONNECTED:
+      Serial.printf("[%u] Connected from IP: %s\n", num, webSocket.remoteIP(num).toString().c_str());
+      Serial.print("My IP address: ");
+      Serial.println(WiFi.localIP());
+      break;
   }
 }
+
 void handleClientRequest(WiFiClient client) {
   float temperature = dht.readTemperature();
   int moistureLevel = analogRead(MOISTURE_PIN);
@@ -36,44 +38,47 @@ void handleClientRequest(WiFiClient client) {
   digitalWrite(TRIGGER_PIN, LOW);
   duration = pulseIn(ECHO_PIN, HIGH);
   distance = duration * 0.034 / 2;
-  client.println("HTTP/1.1 200 OK");
-  client.println("Content-Type: text/plain");
-  client.println();
-  client.print("Temperature: ");
-  client.print(temperature);
-  client.println(" °C");
-  client.print("Moisture Level: ");
-  client.println(moistureLevel);
-  client.print("Distance: ");
-  client.print(distance);
-  client.println(" cm");
-  int numClients =
-      webSocket.broadcastTXT("Temperature: " + String(temperature) +
-                             " °C, Moisture Level: " + String(moistureLevel) +
-                             ", Distance: " + String(distance) + " cm");
+  String response = "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\nConnection: keep-alive\r\n\r\n";
+  response += "<!DOCTYPE HTML>\r\n<html>\r\n";
+  response += "<h1>Temperature: ";
+  response += temperature;
+  response += " °C</h1>\r\n";
+  response += "<h2>Moisture Level: ";
+  response += moistureLevel;
+  response += "</h2>\r\n";
+  response += "<h2>Distance: ";
+  response += distance;
+  response += " cm</h2>\r\n";
+  response += "</html>\r\n";
+  
+  int numClients = webSocket.broadcastTXT(response);
   Serial.printf("Broadcasted to %d clients\n", numClients);
-  client.stop();
-  Serial.println("Client disconnected");
 }
+
 void setup() {
   Serial.begin(9600);
   delay(10);
   WiFi.begin(ssid, password);
+  
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
     Serial.print(".");
   }
+  
   Serial.println("Connected to WiFi");
-  Serial.println(Wifi.localIP())
+  Serial.println(WiFi.localIP());
+  
   webSocket.begin();
   webSocket.onEvent(webSocketEvent);
-  | dht.begin();
+  dht.begin();
   pinMode(TRIGGER_PIN, OUTPUT);
   pinMode(ECHO_PIN, INPUT);
 }
+
 void loop() {
   webSocket.loop();
   WiFiClient client = webSocket.available();
+  
   if (client) {
     handleClientRequest(client);
   }
